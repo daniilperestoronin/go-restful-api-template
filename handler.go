76a994e585660ce7c940dd6bf838c3fbadfd8c0c
perestoronin <perestoronin.daniil@gmail.com>
2018-09-04 18:10:19 +0300

    Add dummy records repository

--- handler.go
+++ handler.go
@@ -6,7 +6,14 @@ import (
 	"net/http"
 )
 
+var records map[int]Record
+
 func recordHandler(w http.ResponseWriter, r *http.Request) {
+	records = make(map[int]Record)
+	records[1] = Record{Id: 1, Title: "Title 1", Text: "Blah Blah Blah Blah 1"}
+	records[2] = Record{Id: 2, Title: "Title 2", Text: "Blah Blah Blah Blah 2"}
+	records[3] = Record{Id: 3, Title: "Title 3", Text: "Blah Blah Blah Blah 3"}
+
 	switch r.Method {
 	case http.MethodGet:
 		get(w, r)
@@ -41,6 +48,6 @@ func bad(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 }
 
-func getRecord() Record {
-	return Record{Id: 1, Title: "Title", Text: "Blah Blah Blah Blah"}
+func getRecord() map[int]Record {
+	return records
 }
